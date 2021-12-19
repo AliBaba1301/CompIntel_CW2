16bec4cb762be16f5b8118d263ca17c35edcf7f1
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2021-12-19 16:08:17 +0000

    Updated Imports and added in NN shell

--- main.py
+++ main.py
@@ -5,7 +5,33 @@ import numpy
 import matplotlib.pyplot as plt
 import random
 import math
-import pytorch
+import torch
+import torch.nn as nn
+
+class Net(torch.nn.Module):
+    # initialise one hidden layer and one output layer
+    def init(self, n_feature, n_hidden, n_output):
+        # call pytorch superclass code to initialise nn
+        super(Net, self).init()
+        # hidden layer
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)
+        self.out = torch.nn.Linear(n_hidden, n_output)  # output layer
+
+    # connect up the layers: the input passes through the hidden, then the sigmoid, then the output layer
+    # x is the data8
+    def forward(self, x):
+        # activation function for hidden layer is relu
+        # data passes through hidden and relu
+        x = F.relu(self.hidden(x))
+        # the result needs to pass through output layer next
+        x = self.out(x)
+        return x
+
+net = Net(n_feature=9, n_hidden=2, n_output=2)  # define the network
+
+#optional printouts about network
+print("printing net")
+print(net)  # net architecture
 
 
 
