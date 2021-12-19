aaa84488655f928f20d41fcc29f0e6e29a901200
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2021-12-19 16:57:31 +0000

    Q1.2 Randomly generate 1100 samples for x1 and x2 within [-1,1]. Calculate the corresponding y values for the 1100 samples. Use 1000 of them as the training dataset, and the other 100 samples as a test dataset.

--- main.py
+++ main.py
@@ -55,13 +55,44 @@ def visualfunction(x1arr, x2arr, title):
     plt.show()
 
 
+# plots a 3D scatter plot
+def scatter3D(x1arr, x2arr, yarr, title):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(x1arr, x2arr, yarr, c=yarr, marker='o')
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('z')
+    ax.set_title(title)
+    plt.show()
+
+
 def main():
+    # creating dataset in order to train and test the model
     # creating 1100 random values for x1 and x2 between -1 and 1
     x1arr = np.random.uniform(-1, 1, 1100)
     x2arr = np.random.uniform(-1, 1, 1100)
+    # creating a list of the function values
+    yarr = function(x1arr, x2arr)
 
     # create a 3d plot of the function
     visualfunction(x1arr, x2arr, 'All Data')
+
+    # creating a list of the training data
+    x1arrtrain = torch.as_tensor(x1arr[:1000], dtype=torch.double)
+    x2arrtrain = torch.as_tensor(x2arr[:1000], dtype=torch.double)
+    yarrtrain = torch.as_tensor(yarr[:1000], dtype=torch.double)
+
+    # plot the training data as a 3D scatter plot
+    scatter3D(x1arrtrain, x2arrtrain, yarrtrain, title='Training Data')
+
+    # creating a list of the testing data
+    x1arrtest = torch.as_tensor(x1arr[1000:1100], dtype=torch.double)
+    x2arrtest = torch.as_tensor(x2arr[1000:1100], dtype=torch.double)
+    yarrtest = torch.as_tensor(yarr[1000:1100], dtype=torch.double)
+
+    # plot testing data as a 3D scatter plot
+    scatter3D(x1arrtest, x2arrtest, yarrtest, title='Testing Data')
 
     # net = Net(n_feature=9, n_hidden=2, n_output=2)  # define the network
     #
