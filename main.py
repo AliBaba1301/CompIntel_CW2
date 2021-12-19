d1ac5fd7f822b3ceb789bdb62cdce25d8721f964
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2021-12-19 16:56:06 +0000

    Q1.1 Final
    Updated the plot to have labels

--- main.py
+++ main.py
@@ -35,29 +35,33 @@ def function(x1, x2):
 
 
 # creates a 3d plot of the function
-def visualfunction(x1arr, x2arr):
+def visualfunction(x1arr, x2arr, title):
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    x = np.linspace(-2.1, 2.1, 100)
-    y = np.linspace(-1.1, 1.1, 100)
-    x, y = np.meshgrid(x, y)
-    z = function(x, y)
+    x1 = np.linspace(-2.1, 2.1, 100)
+    x2 = np.linspace(-1.1, 1.1, 100)
+    x1, x2 = np.meshgrid(x1, x2)
+    y = function(x1, x2)
 
     # create the surface
-    surfaceplot = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.coolwarm, alpha=0.8, linewidth=0,
+    surfaceplot = ax.plot_surface(x1, x2, y, rstride=1, cstride=1, cmap=cm.coolwarm, alpha=0.8, linewidth=0,
                                   antialiased=False)
     ax.view_init(45, 45)
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('y')
+    ax.set_title(title)
     ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
     fig.colorbar(surfaceplot, shrink=0.5)
     plt.show()
 
 
 def main():
-    # creating 100 random values for x1 and x2 between -1 and 1 to visualise function
-    x1arr = np.random.uniform(-1, 1, 100)
-    x2arr = np.random.uniform(-1, 1, 100)
+    # creating 1100 random values for x1 and x2 between -1 and 1
+    x1arr = np.random.uniform(-1, 1, 1100)
+    x2arr = np.random.uniform(-1, 1, 1100)
 
     # create a 3d plot of the function
-    visualfunction(x1arr, x2arr)
+    visualfunction(x1arr, x2arr, 'All Data')
 
     # net = Net(n_feature=9, n_hidden=2, n_output=2)  # define the network
     #
